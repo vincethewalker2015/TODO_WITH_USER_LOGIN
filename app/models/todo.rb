d5da79b08d78e4bb8d26752c757e90cd61cb0716
vincentbwalker <vincethewalker@gmail.com>
2019-04-24 16:01:12 +0000

    Set up TDD Models testing

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,8 +1,8 @@
 class Todo < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true
-  belongs_to :user
-  validates :user_id, presence: true 
+  validates :name, presence: true, length: {minimum: 5}
+  validates :description, presence: true, length: {maximum: 250 }
+  #belongs_to :user
+  #validates :user_id, presence: true 
   
   
 end
