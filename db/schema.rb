3368141059f1e282acefb48630b214d27b0e0009
Vincent <vincethewalker@yahoo.co.uk>
2020-06-03 19:19:25 +0100

    Security Patch Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621114848) do
+ActiveRecord::Schema.define(version: 2018_06_21_114848) do
 
   create_table "todos", force: :cascade do |t|
-    t.string  "name"
-    t.text    "description"
+    t.string "name"
+    t.text "description"
     t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "password_digest"
-    t.boolean  "admin",           default: false
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
