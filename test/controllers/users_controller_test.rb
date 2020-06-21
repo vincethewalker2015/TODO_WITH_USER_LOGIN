a0f1b80f8b04b16e24324d501317f18f2159deb2
Vincent <vincethewalker@yahoo.co.uk>
2020-06-21 19:13:00 +0100

    Added User testing 'part-one'

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create(name: "Vinny", email: "vinnny@yahoo.ca", password: "password")
+    
+  end
+
+  test "should get index" do
+    get users_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_user_url
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count', 1) do
+      post users_url, params: { user: { name: "bobby", email: "bobby@yahoo.ca", password: "password" } }
+    end
+
+    assert_redirected_to user_url(User.last)
+  end
+
+  test "should show user" do
+    get user_url(@user)
+    assert_response :success
+  end
+
+  # test "should get edit" do
+  #   get edit_user_url(@user)
+  #   assert_response :success
+  # end
+
+  # test "should update user" do
+  #   patch user_url(@user), params: { user: {  } }
+  #   assert_redirected_to user_url(@user)
+  # end
+
+  # test "should destroy user" do
+  #   assert_difference('User.count', -1) do
+  #     delete user_url(@user)
+  #   end
+
+  #   assert_redirected_to users_url
+  # end
+end
